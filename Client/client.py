47c80d684aa66d21541950a347aa70b1e3494604
afernander <afernander@eafit.edu.co>
2021-10-04 01:34:41 -0500

    added funtional menu(get and add) and server variables

--- Client/client.py
+++ Client/client.py
@@ -3,83 +3,65 @@ import requests
 import clientVariables
 from os import system, name
 
-valid_commands = ["help", "add", "get", "list"]
-if name == 'nt':
-    _ = system('cls')
-else:
-    _ = system('clear')
 
-while(True): #tet
 
-    print("\nWelcome to our distributed database system for our advanced networking class.\n")
-    print("Input <key,value> must meet the following pattern <unique key,(Name,Age,City)>\n")
-    print("Example -->  <smaring1,(Simon,21,Medellin)>")
-    print("\nList of available commands:")
-    print("add <key,value>")
-    print("get <key>")
-    print("list")
-    print("\nSyntax: tet <command>\n")
-    print("Type enter, then follow the instructions.")
+def main():
+    while(True): #tet
 
-    def input_command():
+        
+    
         choice=input("Enter a command (or enter q to quit): ")
-        if choice.startswith('tet') and choice.split()[1] in valid_commands:
-            choice = choice.split()
-            return choice
+        if choice in clientVariables.VALID_COMMANDS:
+            print("You chose"+ choice) 
         elif choice == 'q':
             quit()
         else:
-            print("You must enter a valid command. Please try again.")
-            input_command()
-        return choice
-    
-    register = [] 
-    command = input_command()
-    if command.split()[1] == "add":
-        cur = ""
-        while not cur.isnumeric():
-            cur = input("Enter you id: ")
-            if not cur.isnumeric():
-                print("You must enter a number.")
-        register.append(cur)
-        cur = input("Enter your name: ")
-        register.append(cur)
-        while not cur.isnumeric():
-            cur = input("Enter your age: ")
-            if not cur.isnumeric():
-                print("You must enter a number.")
-        register.append(cur)
-        cur = input("Enter your city: ")
-        register.append(cur)
-        while not cur.isnumeric():
-            cur = input("Enter your score: ")
-            if not cur.isnumeric():
-                print("You must enter a number.")
-        register.append(cur)
-
+            print("You must enter a valid command. Please try again."+"\n")
+            quit()
         
-    if command.split()[1] == "get":
-        k = command[3:].replace(" ", "")
-        values = k.replace("<", "").replace(">", "").replace("(", "").replace(")", "").split(",")
-    if command.split()[1] == "list":
-        pass #llamar a la función de listar o el procedimiento correspondiente
+        register = [] 
+        command = choice
+        if command == "add":
+            cur = ""
+            while not cur.isnumeric():
+                cur = input("Enter you id: ")
+                if not cur.isnumeric():
+                    print("You must enter a number.")
+            register.append(cur)
+            cur = input("Enter your name: ")
+            register.append(cur)
+            while not cur.isnumeric():
+                cur = input("Enter your age: ")
+                if not cur.isnumeric():
+                    print("You must enter a number.")
+            register.append(cur)
+            cur = input("Enter your city: ")
+            register.append(cur)
+            while not cur.isnumeric():
+                cur = input("Enter your score: ")
+                if not cur.isnumeric():
+                    print("You must enter a number.")
+            register.append(cur)
+
+            to_send = {"id": register[0], "name": register[1], "age": register[2], "city": register[3], "score": register[4]}
+            to_send = str(to_send).replace('\'',"\"")
+            requests.post(clientVariables.URL_ADD, data = to_send, headers = clientVariables.HEADER)
+
+        elif command == "get":
+            cur = ""
+            while not cur.isnumeric():
+                cur = input("Enter the id you want to search: ")
+                if not cur.isnumeric():
+                    print("You must enter a number.")
+            r=requests.get(clientVariables.URL + cur)
+            person = r.text.replace("}","").replace("{","").replace("\'","").split(',')
+            print(f"The id {cur} that you search is from : "+"\n"+person[0]+"\n"+person[1]+"\n"+person[2]+"\n"+person[3])
+            
     
-    url = 'http://127.0.0.1:8006/create'
 
-
-
-    datos = {
-        'id': values[0], 
-        'name': values[1], 
-        'age': values[2], 
-        'city': values[3], 
-        'score': values[4],
-    }
-
-    header = {
-        'content-type': 'application/json'
-    }
-
-    requests.post(url, data = datos, headers = header)
-
-print("Bye!")
+if __name__ == '__main__':
+    print("\nWelcome to our distributed database system for our advanced networking class.\n")
+    print("\nList of available commands:")
+    print("add")
+    print("get")
+    main()
